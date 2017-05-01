6f424b113975b758abea98c1c19d6fd1830eeea0
Galanith <Austin.dodd@Triosstudent.com>
2017-05-01 15:02:45 -0400

    Initial Commit
    Initial Commit

---
+++ app/src/main/java/com/trios/soulseeker/SoulSeekerActivity.java
@@ -0,0 +1,13 @@
+package com.trios.soulseeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoulSeekerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soul_seeker);
+    }
+}
